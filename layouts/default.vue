4f372f8a360ba3ccb8d4b3fd0e79f546c02a65d1
肥客泉 <feikeq@163.com>
2022-02-17 10:29:04 +0800

    设置自动登录

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,21 +7,36 @@
  * @Description: 
 -->
 <template>
-    <div class="nuxt-layout">
-      <BaseMainTop v-if="$store.state.header.type" />
-      <BaseSubTop v-else />
-      <Nuxt />
-      <BaseFooterBottom />
-    </div>
+  <div class="nuxt-layout">
+    <BaseMainTop v-if="$store.state.header.type" />
+    <BaseSubTop v-else />
+    <Nuxt />
+    <BaseFooterBottom />
+  </div>
 </template>
 
 <script>
 // 需要注意的是在默认布局里不要加入头部信息，只是关于<template>标签下的内容统一订制。
 export default {
-  name:"nuxt-layout",
+  name: "nuxt-layout",
   // layouts 里没有asyncData这个方法，其实是所有组件都没有这个只有放到pages智能路由目录的才有asyncData
-  middleware({app, route, store, env, query, params, req, res, redirect, error}) {
+  middleware({
+    app,
+    route,
+    store,
+    env,
+    query,
+    params,
+    req,
+    res,
+    redirect,
+    error,
+  }) {
     // 组件内前置路由守卫
+    console.log(' _');console.log('  __');console.log('  ___');
+    console.log('--- middleware ---');
+    console.log(' _');console.log('  __');console.log('  ___');
+
     // context 服务器上下文
     // 全局守卫前置业务判断
     // store 状态数信息
@@ -30,11 +45,19 @@ export default {
     // params / query 校验参数的合理性
     // console.log('middleware nuxt.config outside-全局守卫前置业务处理')
     // redirect('/login') // 强制跳转到登录页，可做路由鉴权
-  }
-}
+    
+    // 设置自动登录
+    const cookieArr = app.$utils.getCookieArray();
+    if (cookieArr) store.dispatch("sys/setUser", cookieArr);
+  },
+  mounted() {
+    if (process.client) {
+      console.log('客户端加载')
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
 
